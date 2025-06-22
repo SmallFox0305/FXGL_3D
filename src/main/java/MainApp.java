7e6a1c138245cc5c0ece791ea3fc5830db941afa
Small_Fox <small_fox@HPZHAN66>
2025-06-22 11:35:28 +0800

    起项目

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,19 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class MainApp extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+    }
+
+    @Override
+    protected void initGame() {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
